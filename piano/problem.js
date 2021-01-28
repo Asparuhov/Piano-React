a31e6d42559c692ed8035ec73de9dae75a7d00a4
krisi01 <chrisisbeast@abv.bg>
2021-01-28 23:57:52 +0200

    fixing problems

---
+++ piano/problem.js
@@ -0,0 +1,5 @@
+function solution(blocks) {
+    
+}
+
+console.log(2,6, 8, 5);
--- piano/src/App.js
+++ piano/src/App.js
@@ -1,17 +1,89 @@
-import React, {useState} from 'react'
-import './App.css';
+import { Component } from "react";
 
+export default class LikeDislike extends Component {
+  state = {
+    likeClicked: false,
+    likeCount: 0,
+    dislikeCount: 0,
+    dislikeClicked: false,
+  };
 
+  render() {
+    return (
+      <>
+        <div>
+          <button
+            onClick={() => {
+              if (!this.state.likeClicked) {
+                if (this.state.dislikeClicked) {
+                  this.setState({
+                    likeCount: this.state.likeCount + 1,
+                    likeClicked: true,
+                    dislikeClicked: false,
+                    dislikeCount: this.state.dislikeCount - 1,
+                  });
+                } else {
+                  this.setState({
+                    likeCount: this.state.likeCount + 1,
+                    likeClicked: true,
+                    dislikeClicked: false,
+                  });
+                }
+              } else {
+                this.setState({
+                  likeClicked: false,
+                  likeCount: this.state.likeCount - 1,
+                });
+              }
+            }}
+            className={this.state.likeClicked ? "liked" : "like-button"}
+          >
+            Like | {this.state.likeCount}
+          </button>
+          <button
+            onClick={() => {
+              if (this.state.dislikeClicked === false) {
+                if (this.state.likeClicked) {
+                  this.setState({
+                    dislikeCount: this.state.dislikeCount + 1,
+                    dislikeClicked: true,
+                    likeClicked: false,
+                    likeCount: this.state.likeCount - 1,
+                  });
+                } else {
+                  this.setState({
+                    dislikeCount: this.state.dislikeCount + 1,
+                    dislikeClicked: true,
+                    likeClicked: false,
+                  });
+                }
+              } else {
+                this.setState({
+                  dislikeClicked: false,
+                  dislikeCount: this.state.dislikeCount - 1,
+                });
+              }
+            }}
+            className={
+              this.state.dislikeClicked ? "disliked" : "dislike-button"
+            }
+          >
+            Dislike | {this.state.dislikeCount}
+          </button>
+        </div>
+        <style>{`
+                    .like-button, .dislike-button {
+                        font-size: 1rem;
+                        padding: 5px 10px;
+                        color:   #585858;
+                    }
 
-import {useHotkeys} from 'react-hotkeys-hook';
-import useSound from 'use-sound';
-const App = props => {
-  
-  return (
-    <div className="App">
-      
-    </div>
-  );
+                    .liked, .disliked {
+                        font-weight: bold;
+                        color: #1565c0;
+                    }
+                `}</style>
+      </>
+    );
+  }
 }
-
-export default App;
